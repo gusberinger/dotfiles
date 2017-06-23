0747d006ddfb4241d9f4c861a46deb3dcef57763
Gus Beringer <gusberinger@gmail.com>
2017-06-23 14:58:21 -0500

    added git configurations

--- symlink.sh
+++ symlink.sh
@@ -4,3 +4,5 @@ ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.kwm ~/.kwm
 ln -sf ~/dotfiles/.khdrc ~/.khdrc
 ln -sf ~/dotfiles/custom.zsh ~/.oh-my-zsh/custom/custom.zsh
+ln -sf /Users/gusberinger/dotfiles/.gitconfig /Users/gusberinger/.gitconfig
+ln -sf /Users/gusberinger/dotfiles/gitignore /Users/gusberinger/.gitignore
