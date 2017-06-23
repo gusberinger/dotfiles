6a68e6f3c732a285ca8161fa816a9947d5caa288
Gus Beringer <gusberinger@gmail.com>
2017-06-22 20:53:24 -0500

    renamed symlinks

--- symlink.sh
+++ symlink.sh
@@ -1,5 +1,5 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/.kwm/ ~/.kwm/
+ln -sf ~/dotfiles/.kwm ~/.kwm
 ln -sf ~/dotfiles/.khdrc ~/.khdrc
