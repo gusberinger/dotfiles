d0f5ea70b6a5bd5c65c4b3ef08d691a10f0cda5b
Gus Beringer <gusberinger@gmail.com>
2017-06-23 11:41:33 -0500

    added ohmyzsh config

---
+++ custom.zsh
@@ -0,0 +1,19 @@
+# You can put files here to add functionality separated per file, which
+# will be ignored by git.
+# Files on the custom/ directory will be automatically loaded by the init
+# script, in alphabetical order.
+
+# For example: add yourself some shortcuts to projects you often work on.
+#
+# brainstormr=~/Projects/development/planetargon/brainstormr
+# cd $brainstormr
+#
+clear;archey
+DISABLE_UPDATE_PROMPT=true
+alias vim='mvim -v'
+alias q='exit'
+alias show_files='defaults write com.apple.finder AppleShowAllFiles YES'
+alias hide_files='defaults write com.apple.finder AppleShowAllFiles NO'
+
+# setup thefuck
+eval $(thefuck --alias)
--- symlink.sh
+++ symlink.sh
@@ -3,3 +3,4 @@ ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.kwm ~/.kwm
 ln -sf ~/dotfiles/.khdrc ~/.khdrc
+ln -sf ~/dotfiles/custom.zsh ~/.oh-my-zsh/custom/custom.zsh
