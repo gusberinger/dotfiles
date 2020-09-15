3c170fe896ac8d00e06df8e72e833d6610f38746
unknown <gusberinger@gmail.com>
2020-09-14 20:10:33 -0500

    Removed default startup message

--- emacs/init.el
+++ emacs/init.el
@@ -26,6 +26,15 @@
     (menu-bar-mode -1)
   (menu-bar-mode 1))
 
+(setq inhibit-startup-message t
+      inhibit-startup-echo-area-message user-login-name
+      inhibit-default-init t
+      ;; Shave seconds off startup time by starting the scratch buffer in
+      ;; `fundamental-mode', rather than, say, `org-mode' or `text-mode', which
+      ;; pull in a ton of packages.
+      initial-major-mode 'fundamental-mode
+      initial-scratch-message nil)
+
 (use-package evil
   :ensure t
   :config
