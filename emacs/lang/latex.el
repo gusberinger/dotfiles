6375feb2941a184f07eefd84a87e47d363a44256
Gus Beringer <gusberinger@gmail.com>
2020-09-19 14:21:05 -0500

    Created build-on-save-mode

--- emacs/lang/latex.el
+++ emacs/lang/latex.el
@@ -13,35 +13,13 @@
 (setq TeX-PDF-mode t
       auctex-latexmk-inherit-TeX-PDF-mode t
       reftex-plug-into-AUCTeX t)
-(defun LaTeX-build ()
-  (interactive)
-  (progn
-    (let ((TeX-save-query nil))
-      (TeX-save-document (TeX-master-file)))
-    (TeX-command latex-build-command 'TeX-master-file -1)))
-
-(defvar latex-build-command (if (executable-find "latexmk") "LatexMk" "LaTeX"))
-(defvar latex-nofill-env '("equation"
-                           "equation*"
-                           "align"
-                           "align*"
-                           "tabular"
-                           "tikzpicture"))
-
-;; PDF Tools
-(use-package pdf-tools
-  :ensure t
-  :config
-  (custom-set-variables
-    '(pdf-tools-handle-upgrades nil)) ; Use brew upgrade pdf-tools instead.
-  (setq pdf-info-epdfinfo-program "/usr/local/bin/epdfinfo"))
-(pdf-tools-install)
 
 ;; Bindings
 (my-local-leader-def '(normal emacs) LaTeX-mode-map
   "a" 'TeX-command-run-all
   "b" 'LaTeX-build
   "c" 'reftex-citation
+  "z" 'LaTeX-build-on-save-mode
 
   "r" '(:ignore t :which-key "Reftex")
   "rc"    'reftex-citation
@@ -57,3 +35,29 @@
   "rt"    'reftex-toc
   "rT"    'reftex-toc-recenter
   "rv"    'reftex-view-crossref)
+
+;; PDF Tools
+(use-package pdf-tools
+  :ensure t
+  :config
+  (custom-set-variables
+    '(pdf-tools-handle-upgrades nil)) ; Use brew upgrade pdf-tools instead.
+  (setq pdf-info-epdfinfo-program "/usr/local/bin/epdfinfo"))
+(pdf-tools-install)
+
+;; Compilation Settings
+(defun LaTeX-build ()
+  (interactive)
+  (progn
+    (let ((TeX-save-query nil))
+      (TeX-save-document (TeX-master-file)))
+    (TeX-command latex-build-command 'TeX-master-file -1)))
+(defvar latex-build-command (if (executable-find "latexmk") "LatexMk" "LaTeX"))
+
+(define-minor-mode LaTeX-build-on-save-mode
+  "Runs latexmk on file after save"
+  :lighter "BoS"
+  (if LaTeX-build-on-save-mode
+  (progn  (make-local-variable 'after-save-hook)
+      (add-hook 'after-save-hook 'LaTeX-build nil t))
+    (kill-local-variable 'after-save-hook)))
