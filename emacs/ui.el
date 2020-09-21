7dcf98e98fe4d7ec3f649513d87e5d6e6218a8a2
Gus Beringer <gusberinger@gmail.com>
2020-09-21 15:57:37 -0500

    Configured desktop-save instead of manually loading theme.

--- emacs/ui.el
+++ emacs/ui.el
@@ -3,11 +3,11 @@
 (global-visual-line-mode 1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+(desktop-save-mode 1)
+
 
 (use-package base16-theme
-  :ensure t
-  :config
-  (load-theme 'base16-solarized-light t))
+  :ensure t)
 
 (use-package treemacs
   :ensure t)
