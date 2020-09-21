db9cdf14c9e9b2d4416e563b2b8d4cc1bd55c97b
Gus Beringer <gusberinger@gmail.com>
2020-09-21 01:02:35 -0500

    Added zprofile got rid of dot in gitconfig

--- setup.sh
+++ setup.sh
@@ -1,9 +1,14 @@
+# git
+ln -sv ~/dotfiles/gitconfig ~/.gitconfig
+
 # emacs
 ln -sv ~/dotfiles/emacs/init.el ~/.emacs.d/
 
 # zsh
 git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
 ln -sv ~/dotfiles/.zshrc ~/.zshrc
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    ln -sv ~/dotfiles/zprofile ~/.zprofile
 
 # vimrc
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
